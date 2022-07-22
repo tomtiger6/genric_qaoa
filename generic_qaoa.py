fa2f066b00ccab9dc21f40dfe67253b54954481e
Snir Bachar <snir@quantum-machines.co>
2022-07-22 16:41:00 +0300

    finished Clause impl

--- generic_qaoa.py
+++ generic_qaoa.py
@@ -2,6 +2,7 @@ import copy
 from typing import Callable, List
 import numpy as np
 from qiskit import QuantumCircuit
+import qiskit
 from qiskit.circuit import Instruction
 from sympy import *
 
@@ -22,43 +23,53 @@ class Clause(object):
     def msb(self):
         return max(self.one_literals + self.zero_literals)
 
-    @property
-    def as_circuit(self) -> QuantumCircuit:
+    def as_circuit(self, angle) -> QuantumCircuit:
         one_symbols = [(1-symbols('z'+str(l))) for l in self.one_literals]
         zero_symbols = [(1+symbols('z'+str(l))) for l in self.zero_literals]
-        crr = 1
+        H = 1
         for sym  in one_symbols:
-            crr *= sym
+            H *= sym
         for sym  in zero_symbols:
-            crr *= sym
-        H = crr.expand()
-        return self.to_circut(H)
+            H *= sym
+        return self.to_circut(H.expand(), self.size, angle)
 
     @staticmethod
-    def to_circut(symbol) -> QuantumCircuit:
+    def to_circut(symbol, size, angle) -> QuantumCircuit:
         args = Add.make_args(symbol)
-        qc = QuantumCircuit()
+        qc = QuantumCircuit(size)
         for arg in args:
-            # qc.append(Clause.to_z_rotation(arg))
-            (Clause.to_z_rotation(arg))
+            result = Clause.to_z_rotation(arg, size, angle)
+            if result is not None:
+                qc.append(result, range(size))
+        return qc
     
     @staticmethod
-    def to_z_rotation(arg: Symbol) -> QuantumCircuit:
-        qc = QuantumCircuit()
+    def to_z_rotation(arg: Symbol, size, angle) -> QuantumCircuit:
+        qc = QuantumCircuit(size)
+        if arg == 1:
+            return None
         terms = Mul.make_args(arg)
         _terms = terms[1:] if terms[0] == -1 else terms
+        if len(_terms) == 1:
+            qc.rz(angle, Clause.to_qubit_index(_terms[0]))
+            return qc
         for i in range(len(_terms)-1):
-            qc.append(Clause.to_cnot(_terms[i],_terms[i+1]))
-        qc.append(Clause.to_z_rotation(_terms[-1]))
-        for i in range(len(_terms)-1):
-            qc.append(Clause.to_cnot(_terms[i],_terms[i+1]))
-        
-    
+            qc.append(Clause.to_cnot(_terms[i],_terms[i+1], size), range(size))
+        qc.rz(angle, Clause.to_qubit_index(_terms[-1]))
+        for i in reversed(range(len(_terms)-1)):
+            qc.append(Clause.to_cnot(_terms[i],_terms[i+1], size), range(size))
+        return qc
 
+    @staticmethod
+    def to_cnot(control, target, size):
+        qc = QuantumCircuit(size)
+        control_index, target_index = Clause.to_qubit_index(control), Clause.to_qubit_index(target)
+        qc.cnot(control_index, target_index)
+        return qc
 
-
-clause = Clause([0,1,2],[3,4,5],1)
-print(clause.as_circuit)
+    @staticmethod
+    def to_qubit_index(term):
+        return int(term.name[1:])
 
 class GenericQAOA(object):
     def __init__(self, clauses: List[Clause], p: int, simulate: bool):
@@ -90,7 +101,7 @@ class GenericQAOA(object):
 
     def add_prepare_gate(self, qc: QuantumCircuit, angle):
         for clause in self.clauses:
-            qc = qc.append(clause.as_circuit)
+            qc = qc.append(clause.as_circuit(angle), self.number_of_bits)
         return qc
 
     def add_mix_gate(self, qc: QuantumCircuit, angle):
@@ -99,7 +110,7 @@ class GenericQAOA(object):
         return qc
 
     def _create_circuit(self):
-        qc = QuantumCircuit(self.p)
+        qc = QuantumCircuit(self.size)
         qc = self.add_hadamard(qc)
         for i in range(0, self.p):
             qc = self.add_prepare_gate(qc, self.gamma[i])
@@ -118,4 +129,6 @@ class GenericQAOA(object):
 
 
 if __name__ == '__main__':
-    pass
+    clause = Clause([0,1,2],[3,4,5],1)
+    qc = clause.as_circuit(0)
+    print(qc.draw())
