90ac26e2d08502fb354b6194a28f6da4d4cfd11a
Snir Bachar <snir@quantum-machines.co>
2022-07-29 15:27:53 +0300

    Almost finished GenericQaoa

--- generic_qaoa.py
+++ generic_qaoa.py
@@ -1,33 +1,58 @@
+from copy import copy
 from typing import List
-import sympy
-from qiskit import QuantumCircuit
 from sympy import *
+from qiskit import Aer
+from qiskit import QuantumCircuit, execute
+from matplotlib import pyplot as plt
+from qiskit.tools import job_monitor
+from qiskit.visualization import plot_histogram
 
 SIMULATOR_ENGINE = 'local_qasm_simulator'
 REAL_ENGINE = ''
 
 
 class QaoaCircut(QuantumCircuit):
-    def add_cnot(self, control, target, size):
+    def __init__(self, *regs):
+        super().__init__(*regs)
+
+    def append_clause_to_circut(self, expanded_H, angle):
+        args = Add.make_args(expanded_H)
+        for _arg in args:
+            self._add_z_rotation(_arg, angle)
+
+    def _add_cnot(self, control, target):
         control_index, target_index = Clause.to_qubit_index(control), Clause.to_qubit_index(target)
         self.cnot(control_index, target_index)
 
-    def add_z_rotation(self, arg, angle, size):
-        if arg == 1:
+    def _add_z_rotation(self, _arg, angle):
+        if _arg == 1:
             return
-        terms = Mul.make_args(arg)
-        _terms = terms[1:] if terms[0] == -1 else terms
+        terms = Mul.make_args(_arg)
+        _terms, angle = (terms[1:], -angle) if terms[0] == -1 else (terms, angle)
         if len(_terms) == 1:
+            self.barrier()
             self.rz(angle, Clause.to_qubit_index(_terms[0]))
+            self.barrier()
             return
-        for i in range(len(_terms) - 1):
-            self.add_cnot(_terms[i], _terms[i + 1], size)
         self.barrier()
+        for i in range(len(_terms) - 1):
+            self._add_cnot(_terms[i], _terms[i + 1])
         self.rz(angle, Clause.to_qubit_index(_terms[-1]))
         for i in reversed(range(len(_terms) - 1)):
-            self.add_cnot(_terms[i], _terms[i + 1], size)
+            self._add_cnot(_terms[i], _terms[i + 1])
         self.barrier()
-        return
+
+    def add_hadamard(self, qubits_indexes):
+        for q_idx in qubits_indexes:
+            self.h(q_idx)
+
+    def add_prepare_gate(self, clauses, angle):
+        for clause in clauses:
+            self.append_clause_to_circut(clause.parse_h(), angle)
+
+    def add_mix_gate(self, angle, qubits_indexes):
+        for q_idx in qubits_indexes:
+            self.rx(2 * angle, q_idx)
 
 
 class Clause(object):
@@ -37,13 +62,14 @@ class Clause(object):
         self.one_literals = one_literals
         self.zero_literals = zero_literals
         self.weight = weight
+        self.width = self.msb + 1
         self.size = len(one_literals) + len(zero_literals)
 
     @property
     def msb(self):
         return max(self.one_literals + self.zero_literals)
 
-    def as_circuit(self, angle) -> QuantumCircuit:
+    def parse_h(self) -> Symbol:
         one_symbols = [(1 - symbols('z' + str(l))) for l in self.one_literals]
         zero_symbols = [(1 + symbols('z' + str(l))) for l in self.zero_literals]
         H = 1
@@ -51,14 +77,12 @@ class Clause(object):
             H *= sym
         for sym in zero_symbols:
             H *= sym
-        return self.to_circut(H.expand(), self.size, angle)
+        return H.expand()
 
-    @staticmethod
-    def to_circut(symbol, size, angle) -> QaoaCircut:
-        args = Add.make_args(symbol)
-        qc = QaoaCircut(size)
-        for _arg in args:
-            qc.add_z_rotation(_arg, size, angle)
+    def as_circuit(self, angle) -> QaoaCircut:
+        qc = QaoaCircut(self.width)
+        H = self.parse_h()
+        qc.append_clause_to_circut(H.expand(), angle)
         return qc
 
     @staticmethod
@@ -70,8 +94,8 @@ class GenericQAOA(object):
     def __init__(self, clauses: List[Clause], p: int, simulate: bool):
         self.clauses = clauses
         self.p = p
-        self.beta = [0] * p
-        self.gamma = [0] * p
+        self.beta = [1] * p
+        self.gamma = [2] * p
         self.shots = 512
         self.backend = SIMULATOR_ENGINE if simulate else REAL_ENGINE
         self.result = None
@@ -81,7 +105,7 @@ class GenericQAOA(object):
 
     @property
     def number_of_bits(self) -> int:
-        return max([clause.msb for clause in self.clauses])
+        return max([clause.msb for clause in self.clauses]) + 1
 
     def run(self):
         for _ in range(self.p):
@@ -94,9 +118,9 @@ class GenericQAOA(object):
             qc.h(q_idx)
         return qc
 
-    def add_prepare_gate(self, qc: QuantumCircuit, angle):
+    def add_prepare_gate(self, qc: QaoaCircut, angle):
         for clause in self.clauses:
-            qc = qc.append(clause.as_circuit(angle), self.number_of_bits)
+            qc.append_clause_to_circut(clause.parse_h(), angle)
         return qc
 
     def add_mix_gate(self, qc: QuantumCircuit, angle):
@@ -105,25 +129,42 @@ class GenericQAOA(object):
         return qc
 
     def _create_circuit(self):
-        qc = QuantumCircuit(self.size)
-        qc = self.add_hadamard(qc)
+        qc = QaoaCircut(self.number_of_bits)
+        qc.add_hadamard(self.qubits_indexes)
         for i in range(0, self.p):
-            qc = self.add_prepare_gate(qc, self.gamma[i])
-            qc = self.add_mix_gate(qc, self.beta[i])
+            qc.add_prepare_gate(self.clauses, self.gamma[i])
+            qc.add_mix_gate(self.beta[i], self.qubits_indexes)
         qc.measure_all()
         return qc
 
+    def __str__(self):
+        return self.circuit.draw().__str__()
+
     def _execute_circuit(self):
-        pass
+        self.result = None  # execute(self.circuit)
+        print(self.result)
 
     def calc_expectation(self):
         pass
 
-    def _update_theta(self):
+    def _opt_results(self):
         pass
+
+    def _update_theta(self):
+        beta, gamma = self._opt_results()
+        self.beta = copy(beta)
+        self.gamma = copy(gamma)
+        self.circuit = self._create_circuit()
 
 
 if __name__ == '__main__':
-    clause = Clause([0, 1, 2], [3, 4, 5], 1)
-    qc = clause.as_circuit(0)
-    print(qc.draw())
+    clause = Clause([0, 2], [1, 4], 1)
+    qaoa = GenericQAOA([clause], 2, true)
+    circ = qaoa.circuit
+    circ.draw(output="mpl")
+    plt.show()
+    job = execute(circ, backend=Aer.get_backend('qasm_simulator'))
+    job_monitor(job, interval=2)
+    answer = job.result().get_counts()
+    plot_histogram(answer)
+    plt.show()
